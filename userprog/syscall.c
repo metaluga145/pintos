17e604e26db4ef1a62792cb1fa24b94c92ce4202
metaluga145 <oyurchenko145@kaist.ac.kr>
2015-10-03 21:32:04 +0900

    testing argument passing

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -5,6 +5,7 @@
 #include "threads/thread.h"
 
 static void syscall_handler (struct intr_frame *);
+static void sys_write(struct intr_frame *f);
 
 void
 syscall_init (void) 
@@ -13,8 +14,33 @@ syscall_init (void)
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler (struct intr_frame *f)
 {
-  printf ("system call!\n");
-  thread_exit ();
+	int syscalln;
+	memcpy(&syscalln, f->esp, 4);
+	switch(syscalln)
+	{
+	case SYS_WRITE: sys_write(f); break;
+	default:
+	{
+		printf ("system call!\n");
+		thread_exit ();
+	}
+	}
+  //printf ("system call!\n");
+  //thread_exit ();
+}
+
+
+static void sys_write(struct intr_frame *f)
+{
+	//test argument passing. write to console
+	const char* buf;
+	size_t size;
+
+	memcpy(&buffer, f->esp + 8, 4);
+	memcpy(&size, f->esp + 12, 4);
+
+	putbuf(buffer, asize);
+	f->eax = asize;
 }
