7c7822d23a2980fc5559d3c44bb9cb01c742236b
metaluga145 <oyurchenko145@kaist.ac.kr>
2015-10-03 21:28:47 +0900

    testing argument passing

--- userprog/process.c
+++ userprog/process.c
@@ -87,7 +87,6 @@ process_execute (const char *cmdline)
 	  goto free_all;
 
   /* Create a new thread to execute FILE_NAME. */
-  printf("name? = %s\n", args->argv[0]);
   tid = thread_create (args->argv[0], PRI_DEFAULT, start_process, args);
   /* waiting while loading is finished */
   sema_down(&args->loading_block);
@@ -271,7 +270,6 @@ bool
 load (struct args_tmp* args, void (**eip) (void), void **esp)
 {
 	const char *file_name = args->argv[0];
-	printf("file_name = %s\n", args->argv[0]);
   struct thread *t = thread_current ();
   struct Elf32_Ehdr ehdr;
   struct file *file = NULL;
