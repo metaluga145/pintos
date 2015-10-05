428537ea18983125e3754a3546376a393190c4f7
metaluga145 <oyurchenko145@kaist.ac.kr>
2015-10-05 23:15:28 +0900

    pointers validation (not tested)

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -6,6 +6,8 @@
 #include <lib/kernel/console.h>
 #include <string.h>
 
+static int get_int_32(const void* ptr_)
+
 static void syscall_handler (struct intr_frame *);
 static void sys_write(struct intr_frame *f);
 
@@ -19,29 +21,66 @@ static void
 syscall_handler (struct intr_frame *f)
 {
 
-	int syscalln;
-	memcpy(&syscalln, f->esp, 4);
+	int syscalln = get_int_32(f->esp);
 	switch(syscalln)
 	{
-	case SYS_WRITE: sys_write(f); break;
-	default:
-	{
-		thread_exit ();
+		case SYS_WRITE: f->eax = sys_write(f); break;
+		default:
+		{
+			thread_exit ();
+		}
 	}
-	}
-  //printf ("system call!\n");
-  //thread_exit ();
 }
 
 
-static void sys_write(struct intr_frame *f)
+static int sys_write(unsigned int fd, const char *buf, size_t count);
 {
-	//test argument passing. write to console
-	const char* buf;
-	size_t size;
-	memcpy(&buf, f->esp + 8, 4);
-	memcpy(&size, f->esp + 12, 4);
+	if(!buf || buf+size-1 >= PHYS_BASE || get_int_32(buf) == -1) exit(-1);
+	switch(fd)
+	{
+		case 0: exit(-1);
+		case 1:
+		{
+			size_t written = 0;
+			while(written + 128 < count)
+			{
+				putbuf(buf+written, 128);
+				writen += 128;
+			}
+			putbuf(buf+written, count % 128);
+			return count;
+		}
+		default:
+			printf("sys_write not implemented yet!\n")
+			exit(-1);
+	}
+	return -1;
+}
 
-	putbuf(buf, size);
-	f->eax = size;
+static int get_user(const uint8_t* uaddr)
+{
+	int result;
+	asm("movl $1f, %0; movzbl %1, %0; 1:"
+			: "=&a" (result) : "m" (*uaddr));
+	return result;
+}
+
+static bool put_user(uint8_t* udst, uint8_t byte)
+{
+	int error_code;
+	asm("movl $1f, %0; movb %b2, %1; 1:"
+			: "=&a" (error_code) : "=m" (*udst) : "q" (byte));
+}
+
+static int get_int_32(const void* ptr_)
+{
+	if (ptr_ % 4 || ptr_ > PHYS_BASE) exit(-1);
+	uint8_t *ptr = ptr_;
+	int i;
+	for (i = 0; i < 4; ++i)
+	{
+		if (get_user((uint32_t*)(ptr+i)) == -1)
+			exit(-1);
+	}
+	return *((int *)ptr);
 }
