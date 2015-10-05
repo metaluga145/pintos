3d30200e058ce2a475e32a5f872b8943d063868d
metaluga145 <oyurchenko145@kaist.ac.kr>
2015-10-05 19:56:13 +0900

    children list implemented

--- threads/thread.h
+++ threads/thread.h
@@ -4,6 +4,7 @@
 #include <debug.h>
 #include <list.h>
 #include <stdint.h>
+#include "userprog/process.h"
 
 /* States in a thread's life cycle. */
 enum thread_status
@@ -96,6 +97,7 @@ struct thread
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
     uint32_t *pagedir;                  /* Page directory. */
+    struct process* proc;
 #endif
 
     /* Owned by thread.c. */
